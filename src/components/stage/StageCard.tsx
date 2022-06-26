29d891ea3c89c382a6c88c1682a0c8749ff767ba
marko2000 <mmarkom2000@gmail.com>
2022-06-26 03:36:27 +0200

    Fixed style for Stages page

--- src/components/stage/StageCard.tsx
+++ src/components/stage/StageCard.tsx
@@ -9,38 +9,39 @@ const StageCard: React.FC<{ stage: Stage }> = ({ stage }) => {
     const authentication = useAuthentication();
 
     return(
-        <>
             <IonCard className="stage-card">
-                <IonCardHeader className="picture">
-                    <IonImg src={stage.image} className="img" />
-                </IonCardHeader>    
+                <IonImg src={stage.image} className="img" /> 
                 <IonCardContent className="stage-content">
-                    <br />
                     <IonLabel className="stage-name" color="grey">
                         {stage.name}
                     </IonLabel>
                     {authentication.authenticatedUser && authentication.role === "admin" && (<StageCardAdminControls stage={stage} />)}
                 </IonCardContent>
                 <IonRow className="social-stage" color="red">
+                    <div className="stage-info">
                     <IonIcon icon={peopleOutline} className="icon-stage">
                         {" "}
                     </IonIcon>
                     <IonLabel className="stage-tabs">
                         {stage.capacity}
                     </IonLabel>
+                    </div>
+                    <div className="stage-info">
                     <IonIcon icon={cashOutline} className="icon-stage" />
                     <IonLabel className="stage-tabs">
                         {stage.sponsor}
                     </IonLabel>
+                    </div>
+                    <div className="stage-info">
                     <IonIcon icon={locationOutline} className="icon-stage">
                         {" "}
                     </IonIcon>
-                    <IonLabel className="stage-tabs-location">
+                    <IonLabel className="stage-tabs">
                         {stage.location}
                     </IonLabel>
+                    </div>
                 </IonRow>
             </IonCard>
-        </>
     )
 }
 
